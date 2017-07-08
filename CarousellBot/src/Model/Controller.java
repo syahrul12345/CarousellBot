5cc1a2724078eb6023ecf6208984466f164d22c5
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2017-07-09 02:03:14 +0800

    Added search capability and sell capability

--- CarousellBot/src/Model/Controller.java
+++ CarousellBot/src/Model/Controller.java
@@ -1,6 +1,9 @@
 package Model;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Controller {
 private PersonInfo personInfo;
+private JPanel panel;
 	public Controller() {
 		
 	}
@@ -20,14 +24,52 @@ private PersonInfo personInfo;
 		String username = personInfo.getUsername();
 		String password = personInfo.getPassword();
 		String keyword = personInfo.getKeyword();
+		panel = new JPanel();
+		ArrayList<String> list = new ArrayList<String>();
 		chromeDriver.get("https://sg.carousell.com/login/?intent_sell=1&next=/");
 		chromeDriver.findElement(By.id("username")).sendKeys(username);
 		chromeDriver.findElement(By.id("password")).sendKeys(password);
 		chromeDriver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/form/button")).click();
+		
+		if(chromeDriver.getCurrentUrl().equals("https://sg.carousell.com/login/?intent_sell=1&next=/")) {
+			JOptionPane.showMessageDialog(panel, "Incorrect log in info!");
+		}
 		if(keyword.equals("")) {
 			JOptionPane.showMessageDialog(null, "No keyword, wtf u expecting mate");
 		} else {
 			chromeDriver.get("https://sg.carousell.com/search/products/?query=" + keyword);
+			int count2 = chromeDriver.findElements(By.xpath("//h3[@class='pdt-card-username media-heading']")).size();
+			for(int j=0;j<count2;j++) {
+				
+				String text =chromeDriver.findElements(By.xpath("//h3[@class='pdt-card-username media-heading']")).get(j).getText(); //prints out the names of all selelrs with the KEYWORD search
+				list.add(text);
+			}
+		}
+		if(list.contains(username)) {
+			JOptionPane.showMessageDialog(panel, "You already have a listing");
+			chromeDriver.close();
+		} else {
+			String set = "new"; //convert to input field
+			String meet = "meetup"; //convert to input field
+			String descText = "text"; //convert to input field
+			chromeDriver.findElement(By.xpath("//a[@id='navbarSellLink']")).click();
+			chromeDriver.findElement(By.xpath("//input[@placeholder='Item name']")).sendKeys(keyword);
+			chromeDriver.findElement(By.xpath("//div[@id='sellFormCategoryDropdown']")).click();
+			chromeDriver.findElement(By.xpath("//div[contains(text(),'Everything Else')]")).click();
+			chromeDriver.findElement((By.xpath("//div[contains(text(), 'Others')]"))).click();
+			chromeDriver.findElement(By.xpath("//input[@type ='number']")).sendKeys("200.000 this is hardcoded change later");
+			if(set.equals("new")) {
+				chromeDriver.findElement(By.xpath("//label[@for='formSellNewCondition']")).click();
+			} else if(set.equals("used")) {
+				chromeDriver.findElement(By.xpath("//label[@for='formSellUsedCondition']")).click();
+			}
+			chromeDriver.findElement(By.id("formSellDescription")).sendKeys(descText);
+			if(meet.equals("meetup")) {
+				chromeDriver.findElement(By.xpath("//label[@for='formSellDeliveryMeetupOption']")).click();
+			} else if(meet.equals("delivery")) {
+				chromeDriver.findElement(By.xpath("//label[@for='formSellDeliveryMailOption']")).click();
+
+			}
 			
 		}
 	}
--- CarousellBot/src/Model/MainFrame.java
+++ CarousellBot/src/Model/MainFrame.java
@@ -8,6 +8,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -38,6 +39,7 @@ public class MainFrame extends JFrame {
 			}
 			
 		});
+		
 		okBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String username = userField.getText();
